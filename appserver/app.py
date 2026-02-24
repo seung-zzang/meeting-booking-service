588db1dfdff53a2d8d34378f0a090922976ff3fb
seungzzang <nalradenggi@gmail.com>
2026-02-24 14:19:13 +0900

    manage static file in FastAPI

--- appserver/app.py
+++ appserver/app.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from appserver.apps.account.endpoints import router as account_router
 from appserver.apps.calendar.endpoints import router as calendar_router
+from sqladmin import Admin
+from appserver.db import engine
+from sqlalchemy.ext.asyncio import AsyncEngine
+from appserver.admin import include_admin_views
+
 
 app = FastAPI()
 
@@ -8,4 +14,17 @@ def include_routers(_app: FastAPI):
     _app.include_router(account_router)
     _app.include_router(calendar_router)
 
+    # /static 경로로 요청이 들어오면, StaticFiles 앱으로 처리
+    _app.mount("/static", StaticFiles(directory="static"), name="static")
+
+    # /uploads 경로로 요청이 들어오면, StaticFiles 앱으로 처리
+    _app.mount("/uploads", StaticFiles(directory='uploads'), name="uploads")
+
 include_routers(app)
+
+
+# def init_admin(_app: FastAPI, _engine: AsyncEngine):
+#     return Admin(_app, _engine)
+
+# admin = init_admin(app, engine)
+# include_admin_views(admin)
